68e6e18c9ab47fe773340e92b9e0eba52c7a9653
Matej The Tree <matejthetree@gmail.com>
2020-03-30 20:26:09 +0200

    game works

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,19 @@ class PuzzlPage extends StatefulWidget {
 
 class _PuzzlPageState extends State<PuzzlPage> {
   double size = 0;
+
+  Map<int, List<int>> availablePositions = {
+    0: [1, 3],
+    1: [0, 2, 4],
+    2: [1, 5],
+    3: [0, 4, 6],
+    4: [1, 3, 5, 7],
+    5: [2, 4, 8],
+    6: [3, 7],
+    7: [4, 6, 8],
+    8: [5, 7]
+  };
+
   List<int> numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
   @override
@@ -39,13 +52,27 @@ class _PuzzlPageState extends State<PuzzlPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Center(
-          child: Container(
-            width: size ?? 0,
-            height: size ?? 0,
-            color: Colors.yellow,
-            child: Stack(children: _getChildren()),
-          ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            Container(
+              width: size ?? 0,
+              height: size ?? 0,
+              color: Colors.yellow,
+              child: Stack(
+                children: _getChildren(),
+              ),
+            ),
+            RaisedButton(
+              child: Text(
+                "SHUFFLE ME",
+                style: TextStyle(fontSize: 20),
+              ),
+              onPressed: () => setState(() {
+                numbers.shuffle();
+              }),
+            )
+          ],
         ),
       ),
     );
@@ -57,6 +84,7 @@ class _PuzzlPageState extends State<PuzzlPage> {
               alignment:
                   _calculateAlignmentForIndex(numbers.indexOf(numberInt)),
               child: Number(
+                onTap: () => _onNumberTaped(numberInt),
                 number: numberInt,
                 size: size / 3,
               ),
@@ -64,7 +92,7 @@ class _PuzzlPageState extends State<PuzzlPage> {
         .toList();
   }
 
-  _calculateAlignmentForIndex(int index) {
+  Alignment _calculateAlignmentForIndex(int index) {
     double x, y;
     if (index <= 2) {
       y = -1;
@@ -75,7 +103,6 @@ class _PuzzlPageState extends State<PuzzlPage> {
     }
 
     var factor = index % 3;
-    print('index $index got factor $factor');
     if (factor == 0) {
       x = -1;
     } else if (factor == 1) {
@@ -86,28 +113,62 @@ class _PuzzlPageState extends State<PuzzlPage> {
 
     return Alignment(x, y);
   }
+
+  // [1,2,3,4,5,6,7,8,9]
+  // 6=> index 5
+  // 9=> index 8
+  // izbacimo ih
+  // [1,2,3,4,5,7,8,]
+  //
+
+  _onNumberTaped(int selectedNumber) {
+    var selectedIndex = numbers.indexOf(selectedNumber);
+    var indexOfNine = numbers.indexOf(9);
+    bool canChange = availablePositions[indexOfNine].contains(selectedIndex);
+    if (canChange) {
+      setState(() {
+        numbers.remove(9);
+        numbers.remove(selectedNumber);
+        if (indexOfNine < selectedIndex) {
+          numbers.insert(indexOfNine, selectedNumber);
+          numbers.insert(selectedIndex, 9);
+        } else {
+          numbers.insert(selectedIndex, 9);
+          numbers.insert(indexOfNine, selectedNumber);
+        }
+      });
+    }
+  }
 }
 
 class Number extends StatelessWidget {
   final double size;
   final int number;
+  final VoidCallback onTap;
 
-  const Number({Key key, this.size, this.number}) : super(key: key);
+  const Number({Key key, this.size, this.number, this.onTap}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: size,
-      height: size,
-      decoration: BoxDecoration(border: Border.all()),
-      child: number == 9
-          ? Container()
-          : Center(
-              child: Text(
-                number.toString(),
-                style: TextStyle(fontSize: 35),
+    var is9 = number == 9;
+    return GestureDetector(
+      behavior: HitTestBehavior.opaque,
+      onTap: is9 ? null : onTap,
+      child: Container(
+        width: size,
+        height: size,
+        decoration: BoxDecoration(
+            border: Border.all(),
+            color: is9 ? Colors.orange : Colors.transparent),
+        child: is9
+            ? Container()
+            : Center(
+                child: Text(
+                  (number).toString(),
+                  style: TextStyle(fontSize: 35),
+                ),
               ),
-            ),
+      ),
     );
   }
 }
